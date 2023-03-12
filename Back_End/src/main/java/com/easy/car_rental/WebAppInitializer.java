4dd3aad069cc9516922adff7d8bda5972f022a51
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-03-12 22:09:59 +0530

    Update project

--- Back_End/src/main/java/com/easy/car_rental/WebAppInitializer.java
+++ Back_End/src/main/java/com/easy/car_rental/WebAppInitializer.java
@@ -4,13 +4,16 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 @SpringBootApplication
-public class WebAppInitializer {
+public class WebAppInitializer implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebAppInitializer.class);
@@ -19,5 +22,17 @@ public class WebAppInitializer {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(500000000);
+        return multipartResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
     }
 }
